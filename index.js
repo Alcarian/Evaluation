1bc23dd6f8d534a4347914e6f9325e8660249f10
Alcarian <floa2@msn.com>
2022-10-06 16:53:33 +0200

    ajout du JS

---
+++ index.js
@@ -0,0 +1,20 @@
+// const navLinks = document.querySelectorAll(".click-event");
+// const menuToggle = document.getElementById("offcanvasDarkNavbar");
+// const bsCollapse = new bootstrap.Collapse(menuToggle);
+
+// navLinks.forEach((l) => {
+//   l.addEventListener("click", () => {
+//     bsCollapse.toggle();
+//   });
+// });
+
+//----------------------------------------------------------------------------
+
+// const nav = document.getElementById("offcanvasDarkNavbar");
+// const clicklist = document.querySelector("click-event");
+
+// clicklist.addEventListener("click", () => {
+//   nav.classList.add("show-nav");
+// });
+
+//-----------------------------------------------------------------------
